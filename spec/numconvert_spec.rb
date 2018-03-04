99c4b83b4fc5fbe409fed0e61150c8dafec7d44d
railsfactory-lopamudra <lopamudra@railsfactory.org>
2018-03-04 17:07:25 +0530

    rspec added

--- spec/numconvert_spec.rb
+++ spec/numconvert_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Numconvert do
     expect(Numconvert::VERSION).not_to be nil
   end
 
-  it "should add string contrain special charecter" do
-    expect(Numconvert.add_string('1;2%/,,$$6,3')).to eql(12)
+  it "should add string containing special charecter" do
+    expect(Numconvert.stringadd('1;2%/,,$$6,3')).to eql(12)
   end
 
   it "should return zero if string is empty" do
-    expect(Numconvert.add_string('')).to eql(0)
+    expect(Numconvert.stringadd('')).to eql(0)
   end
 
   it "should throw error if string contains negative value" do
-    expect(Numconvert.add_string('-1;2%/,,$$6,3')).to raise_error("Negative Number is not allowed")
+    expect(Numconvert.stringadd('-1;2%/,,$$6,3')).to raise_error("Negative Number is not allowed")
   end
 end
